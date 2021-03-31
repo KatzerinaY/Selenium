990339dfe5b17ca800a03b67344d0fd8d2786508
Yarush <kateryna.yarush@outlook.com>
2021-03-31 20:24:19 +0200

    1 homework

---
+++ src/test/java/com/example/Selenium/Lesson2/FirstTestOpenBrowser.java
@@ -0,0 +1,43 @@
+package com.example.Selenium.Lesson2;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class FirstTestOpenBrowser {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+
+        driver.navigate().to("https://dict.leo.org/russisch-deutsch/");
+//        driver.findElement(By.name("q")).sendKeys("webdriver");
+//        driver.findElement(By.name("btnG")).click();
+//        wait.until(titleIs("webdriver - search in Google"));
+//        driver.get("http://www.google.com");
+    }
+    @After
+
+    public void stop() {
+        driver.quit();
+        driver = null;
+
+    }
+}
