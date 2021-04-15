e0d49ddb8ed5d77931572e94b971b9348f5dfdc3
Yarush <kateryna.yarush@outlook.com>
2021-04-16 01:57:04 +0200

    homework 11

---
+++ src/test/java/com/example/Selenium/Lesson6/NewUserRegistration.java
@@ -0,0 +1,90 @@
+package com.example.Selenium.Lesson6;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
+public class NewUserRegistration {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+
+//        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
+
+        wait = new WebDriverWait(driver, 10);
+    }
+
+
+    @Test
+    public void UserRegistration() throws InterruptedException {
+
+        driver.get("http://localhost/litecart");
+
+        WebElement LoginForm = wait.until(visibilityOfElementLocated(By.name("login_form")));
+        LoginForm.findElement(By.cssSelector("[href]")).click();
+
+
+        WebElement accountForm = wait.until(visibilityOfElementLocated(By.id("create-account")));
+
+        accountForm.findElement(By.name("tax_id")).sendKeys("123456789");
+        accountForm.findElement(By.name("company")).sendKeys("McDonald's");
+        accountForm.findElement(By.name("firstname")).sendKeys("Jim" );
+        accountForm.findElement(By.name("lastname")).sendKeys("Smith");
+        accountForm.findElement(By.name("address1")).sendKeys("123 Avenue, 20");
+        accountForm.findElement(By.name("address2")).sendKeys("Old-park-str, 8a");
+        accountForm.findElement(By.name("postcode")).sendKeys("55277");
+        accountForm.findElement(By.name("city")).sendKeys("New-York");
+
+        WebElement country_code = accountForm.findElement(By.cssSelector("select[name=country_code]"));
+        Select selectC = new Select(country_code);
+        selectC.selectByValue("US");
+
+        WebElement zone_code = accountForm.findElement(By.cssSelector("select[name=zone_code]"));
+        Select selectZ = new Select(zone_code);
+        selectZ.selectByValue("FL");
+
+        WebElement phone = accountForm.findElement(By.name("phone"));
+        phone.sendKeys("+10555333696");
+        String textEmail = "Jm"+ System.currentTimeMillis() + "@m.com";
+        accountForm.findElement(By.name("email")).sendKeys(textEmail);
+        String textPassword = "Jim999Smith";
+        accountForm.findElement(By.name("password")).sendKeys(textPassword);
+        accountForm.findElement(By.name("confirmed_password")).sendKeys(textPassword);
+
+        accountForm.findElement(By.name("create_account")).click(); //Button
+
+        WebElement navigation = wait.until(visibilityOfElementLocated(By.id("navigation")));
+        navigation.findElement(By.cssSelector("[href$=logout]")).click();
+
+        LoginForm = wait.until(visibilityOfElementLocated(By.name("login_form")));
+        LoginForm.findElement(By.name("email")).sendKeys(textEmail);
+        LoginForm.findElement(By.name("password")).sendKeys(textPassword);
+        LoginForm.findElement(By.name("login")).click();
+
+        navigation = wait.until(visibilityOfElementLocated(By.id("navigation")));
+        navigation.findElement(By.cssSelector("[href$=logout]")).click();
+
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+}
+
+
+
