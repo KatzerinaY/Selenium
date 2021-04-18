1147b54cde04a93b8d6fbff030e056ce5f67bcd6
Yarush <kateryna.yarush@outlook.com>
2021-04-18 15:39:47 +0200

    homework 11 fix

--- src/test/java/com/example/Selenium/Lesson6/NewUserRegistration.java
+++ src/test/java/com/example/Selenium/Lesson6/NewUserRegistration.java
@@ -4,12 +4,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
@@ -20,10 +24,14 @@ public class NewUserRegistration {
     @Before
     public void start() {
 
-//        driver = new FirefoxDriver();
-        driver = new ChromeDriver();
+       // driver = new FirefoxDriver();
 
-        wait = new WebDriverWait(driver, 10);
+       // driver = new ChromeDriver();
+
+        System.setProperty("webdriver.edge.driver", "C:\\Java\\Tools\\msedgedriver.exe");
+        driver = new EdgeDriver();
+
+       wait = new WebDriverWait(driver, 10);
     }
 
 
@@ -38,7 +46,6 @@ public class NewUserRegistration {
 
         WebElement accountForm = wait.until(visibilityOfElementLocated(By.id("create-account")));
 
-        accountForm.findElement(By.name("tax_id")).sendKeys("123456789");
         accountForm.findElement(By.name("company")).sendKeys("McDonald's");
         accountForm.findElement(By.name("firstname")).sendKeys("Jim" );
         accountForm.findElement(By.name("lastname")).sendKeys("Smith");
@@ -46,15 +53,6 @@ public class NewUserRegistration {
         accountForm.findElement(By.name("address2")).sendKeys("Old-park-str, 8a");
         accountForm.findElement(By.name("postcode")).sendKeys("55277");
         accountForm.findElement(By.name("city")).sendKeys("New-York");
-
-        WebElement country_code = accountForm.findElement(By.cssSelector("select[name=country_code]"));
-        Select selectC = new Select(country_code);
-        selectC.selectByValue("US");
-
-        WebElement zone_code = accountForm.findElement(By.cssSelector("select[name=zone_code]"));
-        Select selectZ = new Select(zone_code);
-        selectZ.selectByValue("FL");
-
         WebElement phone = accountForm.findElement(By.name("phone"));
         phone.sendKeys("+10555333696");
         String textEmail = "Jm"+ System.currentTimeMillis() + "@m.com";
@@ -62,6 +60,20 @@ public class NewUserRegistration {
         String textPassword = "Jim999Smith";
         accountForm.findElement(By.name("password")).sendKeys(textPassword);
         accountForm.findElement(By.name("confirmed_password")).sendKeys(textPassword);
+
+        // updated 18.04.2021
+        WebElement selectContainer = accountForm.findElement(By.className("select2-container"));
+        selectContainer.click();
+        WebElement itemsList = wait.until(visibilityOfElementLocated(By.className("select2-results")));
+        WebElement usItem = itemsList.findElement(By.cssSelector("li[id$=US]"));
+        usItem.click();
+
+
+        WebElement zone_code = accountForm.findElement(By.cssSelector("select[name=zone_code]"));
+        Select selectZ = new Select(zone_code);
+        selectZ.selectByValue("FL");
+        accountForm.findElement(By.name("tax_id")).sendKeys("123456789");
+
 
         accountForm.findElement(By.name("create_account")).click(); //Button
 
